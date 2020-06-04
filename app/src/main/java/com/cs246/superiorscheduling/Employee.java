ecad73e5232235cb40b190257113b5da376727f4
Boone Mcintosh <boonem88@gmail.com>
2020-06-04 15:00:09 -0600

    Added Employee.java Added EmployeeView.java

---
+++ app/src/main/java/com/cs246/superiorscheduling/Employee.java
@@ -0,0 +1,19 @@
+package com.cs246.superiorscheduling;
+
+import java.time.LocalDate;
+
+public class Employee {
+
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String nickName;
+    private LocalDate birthdate;
+    private boolean manager;
+    private boolean active;
+
+    public Employee(String firstName, String lastName, String nickName, LocalDate birthdate){
+
+    }
+}
---
+++ app/src/main/java/com/cs246/superiorscheduling/EmployeeView.java
@@ -0,0 +1,23 @@
+package com.cs246.superiorscheduling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import java.time.LocalDate;
+
+public class EmployeeView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_view);
+    }
+
+    public void viewSchedule(View view) {
+        Intent intent = new Intent(this, AccountManagerView.class);
+        startActivity(intent);
+    }
+}
