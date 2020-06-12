99fd25f7ec22622b56e4c877f2c0965d9d2ecf34
tarthron1 <kyle.loertscher@gmail.com>
2020-06-12 11:13:34 -0600

    build user class

--- app/src/main/java/com/cs246/superiorscheduling/model/User.java
+++ app/src/main/java/com/cs246/superiorscheduling/model/User.java
@@ -9,22 +9,34 @@ public class User {
     private String firstName;
     private String lastName;
     private String nickName;
-    private LocalDate birthdate;
+    private LocalDate birthDate;
     private boolean manager;
-    private boolean active;
+    private boolean active = false;
 
-    public User(String firstName, String lastName, String nickName, LocalDate birthdate){
-
+    public User(String firstName, String lastName, String nickName, LocalDate birthDate){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nickName = nickName;
+        this.birthDate = birthDate;
     }
 
-    public User(String firstName, String lastName, LocalDate birthdate){
-
+    public User(String firstName, String lastName, LocalDate birthDate){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
     }
 
     public void toggleActive(){
-
+        this.active = !this.active;
     }
 
+    public void toggleManager(){
+        this.manager = !this.manager;
+    }
+
+    public boolean isManager(){
+        return manager;
+    }
     public boolean isActive(){
         return active;
     }
