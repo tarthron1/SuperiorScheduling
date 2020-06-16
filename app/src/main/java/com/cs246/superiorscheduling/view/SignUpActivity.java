1ed92e8d42400cb38b6f2b5daab591122c2835e4
Hannah Rogers <rog19008@byui.edu>
2020-06-16 14:19:13 -0600

    Changing addAccount() to pass info back to MainActivity, updating EmployerView layouts

--- app/src/main/java/com/cs246/superiorscheduling/view/SignUpActivity.java
+++ app/src/main/java/com/cs246/superiorscheduling/view/SignUpActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
 
 import com.cs246.superiorscheduling.R;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Date;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -22,18 +25,41 @@ public class SignUpActivity extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
     }
 
+    // collect info and pass back to main activity
     public void addAccount(View view){
-        // get switch result from create account form
-        Switch managerSwitch = findViewById(R.id.manager_switch);
-        //if manager switch is checked deliver EmployerView
-        if(managerSwitch.isChecked()) {
-            Intent employer = new Intent(this, EmployerView.class);
-            startActivity(employer);
-        }
-        //if manager switch is not checked deliver EmployeeView
-        else if(!managerSwitch.isChecked()) {
-            Intent employee = new Intent(this, EmployeeView.class);
-            startActivity(employee);
-        }
+        Intent intent = new Intent(this, MainActivity.class);
+
+        // collect info from view
+        EditText fn = (EditText) findViewById(R.id.first_name);
+        String firstName = fn.getText().toString();
+
+        EditText ln = (EditText) findViewById(R.id.last_name);
+        String lastName = ln.getText().toString();
+
+        EditText nn = (EditText) findViewById(R.id.nick_name);
+        String nickName = nn.getText().toString();
+
+        EditText bd = (EditText) findViewById(R.id.birth_date);
+        String birthDate = bd.getText().toString();
+
+        Switch pos = findViewById(R.id.manager_switch);
+        Boolean manager = pos.isChecked();
+
+        EditText un = (EditText) findViewById(R.id.username);
+        String username = un.getText().toString();
+
+        EditText pass = (EditText) findViewById(R.id.password);
+        String password = pass.getText().toString();
+
+        // Add info to intent
+        intent.putExtra("firstName", firstName);
+        intent.putExtra("lastName", lastName);
+        intent.putExtra("nickName", nickName);
+        intent.putExtra("birthDate", birthDate);
+        intent.putExtra("manager", manager);
+        intent.putExtra("username", username);
+        intent.putExtra("password", password);
+
+        startActivity(intent);
     }
 }
