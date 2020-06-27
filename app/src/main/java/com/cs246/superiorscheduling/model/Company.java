d0191737a025118c42bd88b70c2f277fd4340eac
tarthron1 <kyle.loertscher@gmail.com>
2020-06-26 20:05:11 -0600

    add Id to Company class and change ListArrays to hold Strings.

--- app/src/main/java/com/cs246/superiorscheduling/model/Company.java
+++ app/src/main/java/com/cs246/superiorscheduling/model/Company.java
@@ -10,62 +10,65 @@ public class Company  {
     private String name;
     private String companyID;
     UUID uuid = UUID.randomUUID();
-    ArrayList<User> inactiveEmployeeList = new ArrayList<>();
-    ArrayList<User> activeEmployeeList = new ArrayList<>();
-    ArrayList<User> managerList = new ArrayList<>();
-    ArrayList<Schedule> scheduleList;
-    ArrayList<Request> requestList;
+    ArrayList<String> inactiveEmployeeList = new ArrayList<>();
+    ArrayList<String> activeEmployeeList = new ArrayList<>();
+    ArrayList<String> managerList = new ArrayList<>();
+    ArrayList<String> scheduleList;
+    ArrayList<String> requestList;
 
     public Company(){
 
     }
 
     // Inactive Employee list Getter
-    public ArrayList<User> getInactiveEmployeeList() {
+    public ArrayList<String> getInactiveEmployeeList() {
         return inactiveEmployeeList;
     }
 
     // Inactive Employee list Setter
-    public void setInactiveEmployeeList(ArrayList<User> inactiveEmployeeList) {
+    public void setInactiveEmployeeList(ArrayList<String> inactiveEmployeeList) {
         this.inactiveEmployeeList = inactiveEmployeeList;
     }
 
     // Active Employee list Setter
-    public void setActiveEmployeeList(ArrayList<User> activeEmployeeList) {
+    public void setActiveEmployeeList(ArrayList<String> activeEmployeeList) {
         this.activeEmployeeList = activeEmployeeList;
     }
 
     // Manager list Setter
-    public void setManagerList(ArrayList<User> managerList) {
+    public void setManagerList(ArrayList<String> managerList) {
         this.managerList = managerList;
     }
 
     // Schedule list Getter
-    public ArrayList<Schedule> getScheduleList() {
+    public ArrayList<String> getScheduleList() {
         return scheduleList;
     }
 
     // Schedule list Setter
-    public void setScheduleList(ArrayList<Schedule> scheduleList) {
+    public void setScheduleList(ArrayList<String> scheduleList) {
         this.scheduleList = scheduleList;
     }
 
     // Request list Getter
-    public ArrayList<Request> getRequestList() {
+    public ArrayList<String> getRequestList() {
         return requestList;
     }
 
     // Request list Setter
-    public void setRequestList(ArrayList<Request> requestList) {
+    public void setRequestList(ArrayList<String> requestList) {
         this.requestList = requestList;
     }
+
+//    public void addRequest(Request request){
+//        this.requestList.add(request.getID());
+//    }
 
     // Constructor
     public Company(String name, User manager){
         this.setName(name);
         this.addManager(manager);
         this.companyID = uuid.toString();
-        this.name = uuid.toString();
 
     }
 
@@ -74,28 +77,28 @@ public class Company  {
     }
 
     // Manager list Getter
-    public ArrayList<User> getManagerList() {
+    public ArrayList<String> getManagerList() {
         return managerList;
     }
 
     // Active Employee list Getter
-    public ArrayList<User> getActiveEmployeeList(){
+    public ArrayList<String> getActiveEmployeeList(){
         return activeEmployeeList;
     }
 
     // Adds employee to the list (inactive by default)
     public void addEmployee(User user){
-        this.inactiveEmployeeList.add(user);
+        this.inactiveEmployeeList.add(user.getUid());
     }
 
     // Makes employee active
     public void makeActive(User user){
-        if (inactiveEmployeeList.contains(user) && (!activeEmployeeList.contains(user))){
-            inactiveEmployeeList.remove(user);
-            activeEmployeeList.add(user);
-        } else if (inactiveEmployeeList.contains(user) && (activeEmployeeList.contains(user))){
-            inactiveEmployeeList.remove(user);
-        } else if ((!inactiveEmployeeList.contains(user)) && activeEmployeeList.contains(user)){
+        if (inactiveEmployeeList.contains(user.getUid()) && (!activeEmployeeList.contains(user.getUid()))){
+            inactiveEmployeeList.remove(user.getUid());
+            activeEmployeeList.add(user.getUid());
+        } else if (inactiveEmployeeList.contains(user.getUid()) && (activeEmployeeList.contains(user.getUid()))){
+            inactiveEmployeeList.remove(user.getUid());
+        } else if ((!inactiveEmployeeList.contains(user.getUid())) && activeEmployeeList.contains(user.getUid())){
 
         } else {
 
@@ -104,23 +107,23 @@ public class Company  {
 
     // Makes employee inactive
     public void makeInactive(User user){
-        if (activeEmployeeList.contains(user)){
-            activeEmployeeList.remove(user);
-            if (!inactiveEmployeeList.contains(user)){
-                inactiveEmployeeList.add(user);
+        if (activeEmployeeList.contains(user.getUid())){
+            activeEmployeeList.remove(user.getUid());
+            if (!inactiveEmployeeList.contains(user.getUid())){
+                inactiveEmployeeList.add(user.getUid());
             }
         }
     }
 
     // Adds manager to the list
     public void addManager(User user){
-        managerList.add(user);
+        managerList.add(user.getUid());
     }
 
     // Removes manager from the list
     public void removeManager(User user){
         if (managerList.size() >= 1){
-            managerList.remove(user);
+            managerList.remove(user.getUid());
         }
     }
 
@@ -135,12 +138,12 @@ public class Company  {
     }
 
     // Adds schedule to the list
-    public void addSchedule(Schedule schedule){
-        this.scheduleList.add(schedule);
-    }
-
-    // Adds request to the list
-    public void addRequest(Request request){
-        this.requestList.add(request);
-    }
+//    public void addSchedule(Schedule schedule){
+//        this.scheduleList.add(schedule.getID());
+//    }
+//
+//    // Adds request to the list
+//    public void addRequest(Request request){
+//        this.requestList.add(request.getID());
+//    }
 }
