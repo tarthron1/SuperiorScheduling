a5ff0ab95f69c3d29b076d8aa076b9de890fcc14
tarthron1 <kyle.loertscher@gmail.com>
2020-07-07 19:37:46 -0600

    get data from database for EditScheduleActivity

--- app/src/main/java/com/cs246/superiorscheduling/presenter/EditSchedulePresenter.java
+++ app/src/main/java/com/cs246/superiorscheduling/presenter/EditSchedulePresenter.java
@@ -1,4 +1,57 @@
 package com.cs246.superiorscheduling.presenter;
 
+import com.cs246.superiorscheduling.model.Company;
+import com.cs246.superiorscheduling.model.Schedule;
+import com.cs246.superiorscheduling.model.Shift;
+import com.cs246.superiorscheduling.model.ShiftTime;
+import com.cs246.superiorscheduling.model.User;
+
+import java.util.ArrayList;
+
 public class EditSchedulePresenter {
+    private User currentUser;
+    private ArrayList<Schedule> schedules = new ArrayList<>();
+    private ArrayList<Shift> shifts = new ArrayList<>();
+    private ArrayList<ShiftTime> shiftTimes = new ArrayList<>();
+    private Company currentCompany;
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(User currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    public ArrayList<Schedule> getSchedules() {
+        return schedules;
+    }
+
+    public void setSchedules(ArrayList<Schedule> schedules) {
+        this.schedules = schedules;
+    }
+
+    public ArrayList<Shift> getShifts() {
+        return shifts;
+    }
+
+    public void setShifts(ArrayList<Shift> shifts) {
+        this.shifts = shifts;
+    }
+
+    public ArrayList<ShiftTime> getShiftTimes() {
+        return shiftTimes;
+    }
+
+    public void setShiftTimes(ArrayList<ShiftTime> shiftTimes) {
+        this.shiftTimes = shiftTimes;
+    }
+
+    public Company getCurrentCompany() {
+        return currentCompany;
+    }
+
+    public void setCurrentCompany(Company currentCompany) {
+        this.currentCompany = currentCompany;
+    }
 }
--- app/src/main/java/com/cs246/superiorscheduling/view/EditScheduleActivity.java
+++ app/src/main/java/com/cs246/superiorscheduling/view/EditScheduleActivity.java
@@ -4,17 +4,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.cs246.superiorscheduling.R;
+import com.cs246.superiorscheduling.model.Company;
+import com.cs246.superiorscheduling.model.Schedule;
+import com.cs246.superiorscheduling.model.Shift;
+import com.cs246.superiorscheduling.model.ShiftTime;
+import com.cs246.superiorscheduling.model.User;
+import com.cs246.superiorscheduling.presenter.EditSchedulePresenter;
 import com.cs246.superiorscheduling.presenter.Listener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 // The view used to edit the Schedules
 public class EditScheduleActivity extends AppCompatActivity implements Listener {
 
+    private EditSchedulePresenter presenter = new EditSchedulePresenter();
     private FirebaseAuth mAuth;
     private FirebaseDatabase database;
     private DatabaseReference databaseUser, databaseSchedules;
@@ -23,6 +36,106 @@ public class EditScheduleActivity extends AppCompatActivity implements Listener 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_schedule);
+        getDataFromDatabase();
+    }
+
+    private void getDataFromDatabase() {
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        databaseUser = database.getReference().child("users").child(mAuth.getUid());
+        databaseUser.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                presenter.setCurrentUser(snapshot.getValue(User.class));
+                getSchedulesFromDatabase();
+                getCompanyFromDatabase();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void getCompanyFromDatabase() {
+        DatabaseReference databaseCompany = database.getReference().child("companies").child(presenter.getCurrentUser().getCompanies().get(0));
+        databaseCompany.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                presenter.setCurrentCompany(snapshot.getValue(Company.class));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void getSchedulesFromDatabase() {
+            final ArrayList<Schedule> schedules = new ArrayList<>();
+            databaseSchedules = database.getReference().child("schedule").child(presenter.getCurrentUser().getCompanies().get(0));
+            databaseSchedules.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    for (DataSnapshot schedule: snapshot.getChildren()
+                         ) {
+                        schedules.add(schedule.getValue(Schedule.class));
+                    }
+                    presenter.setSchedules(schedules);
+                    getShiftsFromDatabase();
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+    }
+
+    private void getShiftsFromDatabase() {
+        final ArrayList<Shift> shifts = new ArrayList<>();
+        DatabaseReference databaseShifts = database.getReference().child("shift").child(presenter.getCurrentUser().getCompanies().get(0));
+        databaseShifts.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot shift: snapshot.getChildren()
+                     ) {
+                    shifts.add(shift.getValue(Shift.class));
+
+                }
+                presenter.setShifts(shifts);
+                getShiftTimeFromDatabase();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void getShiftTimeFromDatabase() {
+        final ArrayList<ShiftTime> shiftTimes = new ArrayList<>();
+        DatabaseReference databaseShiftTimes = database.getReference().child("shiftTime").child(presenter.getCurrentUser().getCompanies().get(0));
+        databaseShiftTimes.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot shiftTime: snapshot.getChildren()
+                     ) {
+                    shiftTimes.add(shiftTime.getValue(ShiftTime.class));
+                }
+                presenter.setShiftTimes(shiftTimes);
+                notifyDataReady();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     // Ability to add shifts
