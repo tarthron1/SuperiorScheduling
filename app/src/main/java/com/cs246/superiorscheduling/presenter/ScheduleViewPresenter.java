aa073b75d0406e01eeab7adcc33bc0ed3fc3d420
tarthron1 <kyle.loertscher@gmail.com>
2020-07-16 15:15:46 -0600

    updating ScheduleViewPresenter

--- app/src/main/java/com/cs246/superiorscheduling/presenter/ScheduleViewPresenter.java
+++ app/src/main/java/com/cs246/superiorscheduling/presenter/ScheduleViewPresenter.java
@@ -18,6 +18,7 @@ public class ScheduleViewPresenter implements Listener {
     private ArrayList<Listener> listeners;
     private User user;
     private Company company;
+    private Schedule selectedSchedule;
 
     public ScheduleViewPresenter(String userID, FirebaseDatabase database, Listener listener){
         listeners = new ArrayList<>();
@@ -41,5 +42,53 @@ public class ScheduleViewPresenter implements Listener {
     @Override
     public void notifyNewDataToSave() {
 
+    }
+
+    public ArrayList<Schedule> getSchedules() {
+        return schedules;
+    }
+
+    public void setSchedules(ArrayList<Schedule> schedules) {
+        this.schedules = schedules;
+    }
+
+    public ArrayList<Shift> getShifts() {
+        return shifts;
+    }
+
+    public void setShifts(ArrayList<Shift> shifts) {
+        this.shifts = shifts;
+    }
+
+    public ArrayList<ShiftTime> getShiftTimes() {
+        return shiftTimes;
+    }
+
+    public void setShiftTimes(ArrayList<ShiftTime> shiftTimes) {
+        this.shiftTimes = shiftTimes;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public Schedule getSelectedSchedule() {
+        return selectedSchedule;
+    }
+
+    public void setSelectedSchedule(Schedule selectedSchedule) {
+        this.selectedSchedule = selectedSchedule;
     }
 }
